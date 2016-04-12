1f12714a6d77dd5ee52d6bb37080d5a5e1e67100
System Administrator <root@Sallys-iMac.local>
2016-04-11 21:20:12 -0600

    have challenge benchmarks met, need to continue polishing

---
+++ public/scripts/app.js
@@ -0,0 +1,38 @@
+angular.module('app',[])
+
+angular.module('app')
+  .controller('mainController',["$scope","$http", function($scope, $http){
+
+     L.mapbox.accessToken = 'pk.eyJ1IjoibWF4d2VsbG8iLCJhIjoiY2ltc2Fma2UxMDFpb3ZsbTR2MjJvMmlwcCJ9.-czd-gCxko0qiczeOvUbig';
+
+     var click = document.getElementById('click'),
+       mousemove = document.getElementById('mousemove');
+
+     var map = L.mapbox.map('map', 'mapbox.streets');
+
+     map.on('mousemove click', function(e) {
+       window[e.type].innerHTML = e.containerPoint.toString() + ', ' + e.latlng.toString();
+       $scope.lat = e.latlng.lat
+       $scope.lng = e.latlng.lng
+       $scope.url = 'mapperdev.tomnod.com/chip_api/chip/lat/' + $scope.lat + '/lng/' + $scope.lng
+     });
+
+     map.on('click', function(e){
+       console.log($scope.lat, $scope.lng)
+     })
+
+    //  $scope.fetch = function(){
+    //    console.log('working')
+    //    $http({
+    //      method : 'GET',
+    //      url : $scope.url
+    //    }).then(function(data){
+    //       console.log(data)
+    //    })
+    //  }
+
+    //  $scope.currentCoordinates = document.getElementById('click')
+
+
+
+  }])
